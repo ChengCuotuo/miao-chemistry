7c2ca0adee240073d1bd7de2a75e96a865688e79
Wangod Chunlei (Contractor) <chunlei.wangod+mcdcn@cn.mcd.com>
2025-12-12 14:13:57 +0800

    feat: 增加按钮

--- src/components/MainBack.vue
+++ src/components/MainBack.vue
@@ -2,8 +2,8 @@
   <div class="w-full h-full flex flex-col overflow-clip">
     <CosmicPortal ref="cosmicPortal" container-class="rounded-lg" />
     <div class="flex flex-row items-center justify-center gap-4">
-      <button @click="activatePortal">Activate Portal</button>
-      <button @click="shiftDimensions">Shift Dimensions</button>
+      <RippleButton @click="activatePortal">Activate Portal</RippleButton>
+      <RippleButton @click="shiftDimensions">Shift Dimensions</RippleButton>
     </div>
   </div>
 </template>
@@ -11,6 +11,7 @@
 <script lang="ts" setup>
 import { useTemplateRef } from 'vue';
 import CosmicPortal from './CosmicPortal.vue';
+import RippleButton from './RippleButton.vue';
 
 const cosmicPortal = useTemplateRef('cosmicPortal');
 
