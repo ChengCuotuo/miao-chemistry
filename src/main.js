4b12c500a45bbbf5885e764d3c93b80e3560ff32
chunlei.wang <lei021191@163.com>
2026-08-24 11:11:31 +0800

    feat: 支持复制粘贴

--- src/main.js
+++ src/main.js
@@ -53,6 +53,22 @@ const createWindow = () => {
 // 在菜单中触发
 const menuTemplate = [
   {
+    label: '编辑',
+    submenu: [
+      { role: 'undo', label: '撤销' },
+      { role: 'redo', label: '重做' },
+      { type: 'separator' },
+      { role: 'cut', label: '剪切' },
+      { role: 'copy', label: '复制' },
+      { role: 'paste', label: '粘贴' },
+      ...(process.platform === 'darwin'
+        ? [{ role: 'pasteAndMatchStyle', label: '粘贴并匹配样式' }]
+        : []),
+      { role: 'delete', label: '删除' },
+      { role: 'selectAll', label: '全选' },
+    ],
+  },
+  {
     label: '帮助',
     submenu: [
       {
